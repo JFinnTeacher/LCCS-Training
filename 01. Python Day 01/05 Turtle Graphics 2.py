c72a97f7be2f1497c6293869cdc6b547b2e7db78
Jim Finn <jim.finnjim2001@gmail.com>
2021-05-18 14:51:08 +0100

    Turtle Graphics Part 2
    Second Turtle Graphics Session

---
+++ 01. Python Day 01/05 Turtle Graphics 2.py
@@ -0,0 +1,43 @@
+from turtle import *
+#Breakout 2.1 - Turtle Graphics (Page 51)
+
+# Part01
+# hideturtle()
+# color("red")
+# 
+# line_length = 50
+# forward(line_length)
+# left(90)
+# line_length = line_length + 50
+# forward(line_length)
+# left(90)
+# line_length = line_length + 50
+# forward(line_length)
+# left(90)
+# line_length = line_length + 50
+# forward(line_length)
+
+# Part02
+# hideturtle()
+# color("red")
+# 
+# line_length = 50
+# forward(line_length)
+# left(90)
+# forward(line_length + 50)
+# left(90)
+# forward(line_length + 50)
+# left(90)
+# forward(line_length + 50)
+
+#Part 03 - Exercise 02
+x=50
+y=25
+forward(2*x+y)
+left(90)
+x=2*x
+forward(x+2*y)
+left(90)
+forward(x+2*y)
+left(90)
+forward(2*x+y)
---
+++ 01. Python Day 01/06 Turtle Graphics 03.py
@@ -0,0 +1,49 @@
+# Breakout 2.1 Part 3 - Page 53
+from turtle import *
+from random import *
+# Part 03
+pensize(2)
+color("red")
+
+# angle = 90
+# line_length = 50
+# forward(line_length)
+# angle = angle + 135
+# left(angle)
+# line_length = line_length + 50
+# forward(line_length)
+# angle = angle + 135
+# left(angle)
+# line_length = line_length + 50
+# forward(line_length)
+# angle = angle + 135
+# left(angle)
+# line_length = line_length + 50
+# forward(line_length)
+
+#Part 04
+# angle = 30
+# forward(50)
+# left(angle)
+# angle = angle+10
+# forward(50)
+# right(angle)
+# angle = angle+10
+# forward(50)
+# left(angle)
+# angle = angle+10
+# forward(50)
+
+#Part 05
+from turtle import *
+from random import *
+forward(randint(50,150))
+left(randrange(360))
+forward(randint(50,150))
+left(randrange(360))
+forward(randint(50,150))
+left(randrange(360))
+forward(randint(50,150))
+left(randrange(360))
+forward(randint(50,150))
+left(randrange(360))
