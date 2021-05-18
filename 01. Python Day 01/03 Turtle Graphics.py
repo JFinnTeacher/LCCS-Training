0ec9b6d89f0f7f4a542251c8c0bea23875ffddb4
Jim Finn <jim.finnjim2001@gmail.com>
2021-05-18 12:31:10 +0100

    Turtle Graphics
    Added file on Turtle graphics and removed .replit file

---
+++ 01. Python Day 01/03 Turtle Graphics.py
@@ -0,0 +1,112 @@
+from turtle import *
+# initial Code on Page 14 of LCCS Handout
+
+#V01
+
+# forward(100) #Move forward 100 units
+# left(90) # Rotate left 90 Degrees
+# forward(100)
+# right(45)
+# forward(50)
+# left(90)
+# forward(100)
+
+#V02
+
+# forward(50) #Move forward 100 units
+# left(90) # Rotate left 90 Degrees
+# forward(100)
+# right(90)
+# forward(50)
+# left(90)
+# forward(100)
+
+#V01 - Triangle
+shape("turtle")
+color("purple")
+forward(150)
+left(120)
+forward(150)
+left(120)
+forward(150)
+left(120)
+
+#V02 - square
+shape("turtle")
+color("orange")
+forward(150)
+left(90)
+forward(150)
+left(90)
+forward(150)
+left(90)
+forward(150)
+left(90)
+
+#V03 - Pentagon
+shape("turtle")
+color("red")
+forward(150)
+left(72)
+forward(150)
+left(72)
+forward(150)
+left(72)
+forward(150)
+left(72)
+forward(150)
+left(72)
+
+#V04 - Hexagon
+color("blue")
+pensize(3)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+
+#V06 - Heptagon
+color("teal")
+pensize(4)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+forward(150)
+left(360/7)
+
+#V07 - Octagon
+color("Yellow")
+pensize(4)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
+forward(150)
+left(360/8)
