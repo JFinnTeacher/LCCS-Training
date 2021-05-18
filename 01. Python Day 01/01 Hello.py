539720446fa6674be82c283fe4735594705ca96c
Jim Finn <jim.finnjim2001@gmail.com>
2021-05-18 11:01:53 +0100

    Session 01

---
+++ 01. Python Day 01/01 Hello.py
@@ -0,0 +1,10 @@
+#V01
+
+print("Hello World")
+print("Another Line")
+print("Hello Again")
+print(10+25)
+print(2021) #integer is same as int
+print(1.33) # float is integer with decmial
+
+#V02
---
+++ 01. Python Day 01/02 Escape Sequences.py
@@ -0,0 +1,13 @@
+# Including the \ tells python that everything inside the \ is part of the parent string This only works on the immediate following character
+# and not everything
+print("In the words of Nelson Mandela, \"Education is the most powerful weapon which we can use to change the world\"")
+print("\n")
+#including \n moves the rest to the string to a new line
+print("In the words of Nelson Mandela,\n \t \"Education is the most powerful weapon which we can use to change the world\"")
+
+print("\n")
+#Experiment Page 09
+#A tab to B and C goes to new line
+print("A \tB \nC")
+#Including the backslash in the string by using a double blackslash
+print("C:\\Users\\johndoe\\Documents\\myfile.txt")
