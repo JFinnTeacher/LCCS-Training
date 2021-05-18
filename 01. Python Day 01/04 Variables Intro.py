03466e4dc78ad4c8e6c323cecc24b3ed3e364434
Jim Finn <jim.finnjim2001@gmail.com>
2021-05-18 12:58:02 +0100

    Create 04 Variables Intro.py

---
+++ 01. Python Day 01/04 Variables Intro.py
@@ -0,0 +1,24 @@
+#V01 - First one
+# print("Hi Alex - Your fav colour is red")
+# print("Goodbye Alex")
+
+#V02 - Variable
+# two ways of doing variables - Underscore option is common to Python
+#Cammo style is common to others personName instead of person_name
+#Important is to be consistant with defining variables
+
+# person_name = "John"
+# favourite_colour = "blue"
+# print("Hi", person_name, "- your favourite colour is", favourite_colour)
+# print("Goodbye ", person_name)
+
+#V03 - Inputs
+#Input asks for an input and stores this in the variable
+person_name = input("Enter your name: ")
+favourite_colour = input("What is your favourite colour: ")
+print("Hi", person_name, "- your favourite colour is", favourite_colour)
+print("Goodbye ", person_name)
+
+x = 3+4
+x = x+1
+print(x)
