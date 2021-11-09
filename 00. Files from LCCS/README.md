fe7a6ab20a43d4387fabab69ba80736f842b3c0f
Jim Finn <jim.finnjim2001@gmail.com>
2021-11-09 10:18:20 +0000

    File Restructuring
    Moving Reference Files out leaving works files only

--- 00. Files from LCCS/README.md
+++
@@ -1,2 +0,0 @@
-# lccs-python
-A repo for .py source of examples and exercises referenced in the Python manual
--- 00. Files from LCCS/Section 3 - Strings/Str Ops - syntax2.py
+++
@@ -1,4 +0,0 @@
-# A string index should always be within range
-s = "Hello"
-print(s[5]) # Syntax error - valid indices for s are 0-4
-
